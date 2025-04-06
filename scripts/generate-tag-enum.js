9c4c7e1b459c00a6c9ef9bfc2b4f31d29325bb70
Cody <60776211+CJStandingwater@users.noreply.github.com>
2025-04-06 17:17:21 -0500

    Update generate-tag-enum.js

--- scripts/generate-tag-enum.js
+++ scripts/generate-tag-enum.js
@@ -7,5 +7,14 @@ const output = path.join(__dirname, "../schema/tags.flattened.json");
 const grouped = JSON.parse(fs.readFileSync(source, "utf-8"));
 const flattened = Object.values(grouped).flat();
 
-fs.writeFileSync(output, JSON.stringify(flattened, null, 2));
+const schemaWrapper = {
+  $id: "tags.flattened.json",
+  type: "array",
+  items: {
+    type: "string",
+    enum: flattened
+  }
+};
+
+fs.writeFileSync(output, JSON.stringify(schemaWrapper, null, 2));
 console.log("tags.flattened.json updated from tags.enum.json");
